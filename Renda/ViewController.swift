cb659cade0f1ee487deeea72cae70f37fa803c64
YuiOgawa <yui20010218@gmail.com>
2022-08-26 23:34:34 +0900

    Initial Commit

---
+++ Renda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renda
+//
+//  Created by Yui Ogawa on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
