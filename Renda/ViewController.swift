e781660a1dc0944f121328f8056d089a828b5757
YuiOgawa <yui20010218@gmail.com>
2022-08-27 00:28:36 +0900

    :sparkles: 教科書2完成

--- Renda/ViewController.swift
+++ Renda/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // タップ数を表示するLabelの変数を準備
+    @IBOutlet var countLabel: UILabel!
+    
+    // TAPボタンの変数を準備
+    @IBOutlet var tapButton: UIButton!
+    
+    // タップ数を数える変数を準備
+    var tapCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tapButton.layer.cornerRadius = 125
     }
-
-
+    
+    // TAPボタンが押されたときの動作
+    @IBAction func tapTapButton(){
+        // タップ数を数える変数に +1
+        tapCount = tapCount + 1
+        
+        // タップされた和をラベルに反映
+        countLabel.text = String(tapCount)
+    }
 }
 
